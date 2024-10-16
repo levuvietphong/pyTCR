fd2cc410c2fa6cf875aa4937f0494bd0dadf8f78
Phong Le <levuvietphong@gmail.com>
2024-10-16 19:23:20 -0400

    feat: add functions to load colormaps from cpt files

--- tcr/plots.py
+++ tcr/plots.py
@@ -2,13 +2,17 @@
 Functions for plottings in PyTCR
 """
 
+import os
+import colorsys
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.path as mpath
 from matplotlib import ticker
 from matplotlib.collections import LineCollection
 from matplotlib.colors import LogNorm, LinearSegmentedColormap
+import matplotlib.colors as mcolors
 
+import geopandas as gpd
 from cartopy.feature import ShapelyFeature
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
@@ -462,3 +466,119 @@ def create_buffer_around_POI(lat_poi, lon_poi, radius=1.0):
     point = Point(lon_poi, lat_poi)
     buffer = point.buffer(radius)
     return Polygon(buffer.exterior.coords)
+
+
+def gmtColormap_openfile(cptf, name=None):
+    """Read a GMT color map from an OPEN cpt file
+
+    Parameters
+    ----------
+    cptf : open file or url handle
+        path to .cpt file
+    name : str, optional
+        name for color map
+        if not provided, the file name will be used
+    """
+    # generate cmap name
+    if name is None:
+        name = '_'.join(os.path.basename(cptf.name).split('.')[:-1])
+
+    # process file
+    x = []
+    r = []
+    g = []
+    b = []
+    lastls = None
+    for line in cptf.readlines():
+        ls = line.split()
+
+        # skip empty lines
+        if not ls:
+            continue
+
+        # parse header info
+        if ls[0] in ["#", b"#"]:
+            if ls[-1] in ["HSV", b"HSV"]:
+                colorModel = "HSV"
+            else:
+                colorModel = "RGB"
+            continue
+
+        # skip BFN info
+        if ls[0] in ["B", b"B", "F", b"F", "N", b"N"]:
+            continue
+
+        # parse color vectors
+        x.append(float(ls[0]))
+        r.append(float(ls[1]))
+        g.append(float(ls[2]))
+        b.append(float(ls[3]))
+
+        # save last row
+        lastls = ls
+
+    x.append(float(lastls[4]))
+    r.append(float(lastls[5]))
+    g.append(float(lastls[6]))
+    b.append(float(lastls[7]))
+
+    x = np.array(x)
+    r = np.array(r)
+    g = np.array(g)
+    b = np.array(b)
+
+    if colorModel == "HSV":
+        for i in range(r.shape[0]):
+            # convert HSV to RGB
+            rr, gg, bb = colorsys.hsv_to_rgb(r[i]/360., g[i], b[i])
+            r[i] = rr
+            g[i] = gg
+            b[i] = bb
+    elif colorModel == "RGB":
+        r /= 255.
+        g /= 255.
+        b /= 255.
+
+    red = []
+    blue = []
+    green = []
+    xNorm = (x - x[0])/(x[-1] - x[0])
+    for i in range(len(x)):
+        red.append([xNorm[i], r[i], r[i]])
+        green.append([xNorm[i], g[i], g[i]])
+        blue.append([xNorm[i], b[i], b[i]])
+
+    # return colormap
+    cdict = dict(red=red, green=green, blue=blue)
+    return mcolors.LinearSegmentedColormap(name=name, segmentdata=cdict)
+
+
+def load_cpt_colormap(cptfile, name=None):
+    """Read a color map from a cpt file
+
+    Parameters
+    ----------
+    cptfile : str or open file-like object
+        path to .cpt file
+    name : str, optional
+        name for color map
+        if not provided, the file name will be used
+    """
+    with open(cptfile, 'r') as cptf:
+        return gmtColormap_openfile(cptf, name=name)
+
+
+def shape2grid(x, y, shapefile):
+    # Load the shapefile
+    gdf = gpd.read_file(shapefile)
+    # Create a meshgrid
+    X, Y = np.meshgrid(x-360, y)
+
+    # Check if each point is inside the shapefile
+    points = [Point(x, y) for x, y in zip(X.ravel(), Y.ravel())]
+    inside = [gdf.contains(point).any() for point in points]
+
+    # Reshape the result to the shape of the meshgrid
+    inside = np.array(inside).reshape(X.shape)
+    inside = np.where(inside, 1, np.nan)
+    return inside
