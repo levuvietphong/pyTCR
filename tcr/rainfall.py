1. Replace the inconsistent or chaotic usage of constants throughout the codebase with a uniform approach.   2. Correct the calculation of OMEGA.

tcr/rainfall.py
@@ -9,6 +9,7 @@ import numpy as np
 from tcr import wind as tcr_wind
 from tcr import terrain_boundary as tcr_tb
 from tcr import iodata as tcr_io
+from tcr import constants as tcr_cons
 from tcr.datadir import DATA_DIR
 
 
@@ -16,7 +17,7 @@ def calculate_rainfall_rate(
     nt, latitude, longitude, radius_storm, velocity, radius_storm_secondary,
     velocity_secondary, ut, vt, u850, v850, months, days, hours, monthplot,
     dayplot, hourplot, extent=None, shapefile=None, magfac=1.0, deltax=5,
-    deltay=4, dellatlong=0.05, q900=0.01, eprecip=0.9, wrad=-0.005
+    deltay=4, dellatlong=0.05, q900=0.01, eprecip=tcr_cons.eprecip, wrad=tcr_cons.wrad
 ):
     """
     Computes the surface rain rate distribution (mm/hr) for a specified storm
@@ -99,11 +100,11 @@ def calculate_rainfall_rate(
             bxmin, bxmax, bymin, bymax = tcr_io.get_bbox_from_shapefile(
                 shapefile)
 
-    RHOA_OVER_RHOL = 0.00117  # ratio of air density over water density
+    RHOA_OVER_RHOL = tcr_cons.RHOA_OVER_RHOL  # ratio of air density over water density
     nrm, mrm = np.shape(radius_storm)
     rfac = magfac * (1+np.zeros((nrm, mrm)))
-    pifac = math.acos(-1)/180
-    knotfac = 1852./3600  # convert knots to m/s (1 knots = 0.5144 m/s)
+    pifac = tcr_cons.RAD2DEG  # pi number
+    knotfac = tcr_cons.KNOTS2MPS  # convert knots to m/s (1 knots = 0.5144 m/s)
 
     # Get the length of each event by finding the first 0 element,
     # if non-zero, get all length
@@ -191,7 +192,7 @@ def calculate_etr_swath(
     nt, latitude, longitude, radius_storm, velocity, radius_storm_secondary,
     velocity_secondary, ut, vt, u850, v850, extent=None, shapefile=None,
     magfac=1, deltax=5, deltay=4, dellatlongs=0.15, q900=0.01, timeres=0.5,
-    wrad=-0.005, eprecip=0.9
+    wrad=tcr_cons.wrad, eprecip=tcr_cons.eprecip
 ):
     """
     Calculate the distribution of event total rainfall for a given individual
@@ -259,7 +260,7 @@ def calculate_etr_swath(
                 shapefile)
 
     # Constants
-    RHOA_OVER_RHOL = 0.00117  # rho_air / rho_liquid
+    RHOA_OVER_RHOL = tcr_cons.RHOA_OVER_RHOL  # rho_air / rho_liquid
 
     # Initialize variables
     nrm, mrm = np.shape(radius_storm)
@@ -340,7 +341,7 @@ def generate_rainfall_point(
     plat, plong, latitude, longitude, datearray, velocity,
     radius_storm, velocity_secondary, radius_storm_secondary,
     u850, v850, utrans, vtrans, T600=None, magfac=1.0,
-    q900_constant=0.01, timeres=0.5, wrad=-0.005, eprecip=0.9
+    q900_constant=0.01, timeres=0.5, wrad=tcr_cons.wrad, eprecip=tcr_cons.eprecip
 ):
     """
     Calculate the accumulated rainfall and rain rates at a specified location
@@ -417,8 +418,8 @@ def generate_rainfall_point(
     topores = 360.0 / ntopo  # topo resolution in degree
     toporesi = 1.0 / topores  # inverse of topo resolution
     sfac = 1.0 / (topores * 60.0 * 1852)  # factor converting degree to m
-    pifac = math.acos(-1) / 180  # pi number
-    knotfac = 1852.0 / 3600  # convert nautical mile to m/s
+    pifac = tcr_cons.RAD2DEG  # pi number
+    knotfac = tcr_cons.KNOTS2MPS  # convert nautical mile to m/s
     m, n = ut.shape  # m: num of storm; n: num of time steps
 
     if np.min(plong) < 0:
@@ -479,7 +480,7 @@ def generate_rainfall_point(
     date_record = np.reshape(datetimes_numpy, wq.shape)
 
     # Constants
-    RHOA_OVER_RHOL = 0.00117  # rho_air / rho_liquid
+    RHOA_OVER_RHOL = tcr_cons.RHOA_OVER_RHOL # rho_air / rho_liquid
 
     # Calculate rain rate (mm/hr)
     rainrate = eprecip * 1000 * 3600 * RHOA_OVER_RHOL * wq

tcr/terrain_boundary.py
@@ -5,6 +5,7 @@ Functions for topography and boundary layers in PyTCR
 import math
 import numpy as np
 from tcr import iodata as tcr_io
+from tcr import constants as tcr_cons
 from tcr.datadir import DATA_DIR
 
 
@@ -47,7 +48,7 @@ def calculate_distance_to_track(
     dy : numpy.ndarray
         Distance components in the y direction (km).
     """
-    pi_factor = np.pi / 180  # Use numpy's pi constant
+    pi_factor = tcr_cons.RAD2DEG  # Use numpy's pi constant
     shape = (num_storms, storm_length, num_x_points, num_y_points)
     dx = np.zeros(shape)
     dy = np.zeros(shape)
@@ -192,7 +193,7 @@ def estimate_topographic_height(bxmin, bxmax, bymin, bymax, dellatlong):
     ntopo, _ = np.shape(bathymetry)  # number of grid point in x direction
     topo_resolution = 360 / ntopo
     topo_resolution_inv = 1 / topo_resolution
-    pi_factor = math.pi / 180  # pi number
+    pi_factor = tcr_cons.RAD2DEG
     # scale factor converting nautical mile (degree) to meter
     scale_factor = 1.0 / (topo_resolution * 60.0 * 1852)
 

tcr/wind.py
@@ -10,6 +10,7 @@ import glob
 import numpy as np
 from tcr import terrain_boundary as tcr_tb
 from tcr import iodata as tcr_io
+from tcr import constants as tcr_cons
 from tcr.datadir import DATA_DIR
 
 
@@ -762,15 +763,16 @@ def calculate_upward_velocity_field(
 
     plong = plong + 360 if plong[0] < 0 else plong
 
-    pifac = math.pi / 180
+    pifac = tcr_cons.RAD2DEG
     dfac = 60 * 1.852
 
     # scale factor converting nautical miles to meters (using 0.25 degree resolution).
     sfac = 1 / (0.25 * 60 * 1852)
-    knotfac = 1852. / 3600  # convert knots to m/s (1 knots = 0.5144 m/s)
+    knotfac = tcr_cons.KNOTS2MPS  # convert knots to m/s (1 knots = 0.5144 m/s)
     ut = ut * knotfac   # convert to m/s
     vt = vt * knotfac   # convert to m/s
-    omega = math.pi / (6 * 3600)
+    #omega = math.pi / (6 * 3600)
+    omega = tcr_cons.OMEGA  # Earth angular velocity parameter (rad/s)
     latfac = (latitude[0] / (abs(latitude[0]) + 1e-8) if latitude.ndim == 1
               else latitude[0, 0] / (abs(latitude[0, 0]) + 1e-8))
 
@@ -1128,11 +1130,11 @@ def calculate_upward_velocity_time_series(
     timeresi = 1.0 / (3600 * timeres)
 
     # convert degree to km (1 nautical mile = 1/60 degree = 1.852 km)
-    pifac = math.acos(-1) / 180  # pi number
+    pifac = tcr_cons.RAD2DEG  # pi number
     dfac = 60 * 1.852
     sfac = 1.0 / (0.25 * 60.0 * 1852)
-    knotfac = 1852.0 / 3600  # convert knots to m/s (1 knots = 0.5144 m/s)
-    omega = math.acos(-1) / (6 * 3600)  # Earth angular velocity parameter
+    knotfac = tcr_cons.KNOTS2MPS  # convert knots to m/s (1 knots = 0.5144 m/s)
+    omega = tcr_cons.OMEGA  # Earth angular velocity parameter
 
     se = np.nanmax(velocity_secondary)  # for secondary eyewalls
     ntime = int(timelength / timeres + 1)
@@ -1470,7 +1472,7 @@ def estimate_radius_wind(ds, lat_tracks, vmax_tracks, id_tracks,
                 vsin = vmax_tracks[ind, jnd]
                 # Skip at equator (coriolis force fc = 0) or vs = 0
                 if alats != 0 and vsin > 0:
-                    fc1 = 1.45e-4 * np.abs(alats * 0.0175)
+                    fc1 = 2 * tcr_cons.OMEGA * np.sin(abs(alats) * tcr_cons.RAD2DEG)
                     _, rm, _ = integrate_outer_wind_profile(vsin, fc1, ro, wc, cdouter, nouter)
                 else:
                     rm = 0